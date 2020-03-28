d7b83caf7aa152acc2cc5b10472080f7e7592303
Julio Cesar <juliocesarbiz@hotmail.com>
2020-03-27 22:52:41 -0300

    Front-end do app

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -43,7 +43,7 @@ module.exports = {
     },
 
     async delete(request, response){
-        const{id} = request.paramas;
+        const{id} = request.params;
         const ong_id = request.headers.authorization;
 
         const incident = await connection('incidents')
